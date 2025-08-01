c708e35771aa4dacf3be57cec0c82ada7f7c5add
mash-pg <ymanshou@gmail.com>
2025-08-02 03:51:38 +0900

    test修正

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,8 @@ module.exports = {
     '<rootDir>/tests/**/*.test.(ts|tsx)',
   ],
   transform: {
-    '^.+\.(ts|tsx)$': 'babel-jest',
+    '^.+\.(ts|tsx)$': ['babel-jest', {
+      presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+    }],
   },
 };
