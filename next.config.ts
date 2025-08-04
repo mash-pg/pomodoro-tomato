63dbadbe9e26c47dfc4656f60f3fca4c333460f7
mash-pg <ymanshou@gmail.com>
2025-08-05 00:26:18 +0900

    通知機能処理修正

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,20 @@ const pwaConfig = withPWA({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development", // 開発環境ではPWAを無効にする
+  buildExcludes: [
+    /app-build-manifest\.json$/,
+    /app-path-routes-manifest\.json$/,
+    /app-route-manifest\.json$/,
+    /build-manifest\.json$/,
+    /react-loadable-manifest\.json$/,
+    /routes-manifest\.json$/,
+    /server-build-manifest\.json$/,
+    /static-build-manifest\.json$/,
+    /_middleware\.js$/,
+    /_middleware\.js\.map$/,
+    /_middleware\.ts$/,
+    /_middleware\.ts\.map$/,
+  ],
 });
 
 export default pwaConfig(nextConfig);
